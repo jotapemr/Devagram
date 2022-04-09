ce6d9cd86570815fda8243c6ed76b294dc40cdae
jotapemr <101481431+jotapemr@users.noreply.github.com>
2022-04-09 20:45:09 -0300

    Conclusão schemas

---
+++ pages/Cadastro.ts
@@ -0,0 +1,39 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+import type {RespostaPadraoMsg} from '../type/RespostaPadraoMsg'
+import type {UsuarioRequisicao} from '../type/UsuarioRequisicao'
+import { UsuarioModel } from '../models/UsuarioModels';
+import md5 from 'md5';
+import { conectarMongoDB } from '../midllewares/conectarMongoDB';
+
+const endpointCadastro = async (req : NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+
+    if(req.method === 'POST'){
+        const usuario = req.body as UsuarioRequisicao
+
+        if(!usuario.nome || usuario.nome.length < 2){
+            return res.status(400).json({erro: 'Nome inválido'})
+        }
+
+        if(!usuario.email || usuario.email.length < 5 || usuario.email.includes('@') || usuario.email.includes('.')){
+            return res.status(400).json({erro: 'Email inválido'})
+        }
+
+        if(!usuario.senha || usuario.senha.length < 4) {
+            return res.status(400).json({erro : 'Senha inválida'})
+        }   
+        //validar se ja existe o mesmo email
+        const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email})
+
+        //salvar no banco de dados
+
+        const usuarioASerSalvo = {
+            nome : usuario.nome,
+            email : usuario.email,
+            senha : md5(usuario.senha)
+        }
+        await UsuarioModel.create(usuario);
+        return res.status(200).json({msg : 'Usuário criado com sucesso'})
+    }
+    return res.status(405).json({erro: 'Método informado não é valido'})
+}
+export default conectarMongoDB(endpointCadastro)
--- pages/login.ts
+++ pages/login.ts
@@ -1,17 +1,21 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import {conectarMongoDB} from '../midllewares/conectarMongoDB'
 import type {RespostaPadraoMsg} from '../type/RespostaPadraoMsg'
+import md5 from 'md5';
+import { UsuarioModel } from '../models/UsuarioModels';
 
-const endpointLogin = (
+const endpointLogin = async (
     req : NextApiRequest,
     res : NextApiResponse<RespostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
 
-        if(login === 'admimadmim@.com' &&
-        senha === 1234){
-            return res.status(200).json({msg : 'Usuário autenticado'})
+        const usuariosEncontrados = await UsuarioModel.find({email: login, senha: md5(senha)})
+
+        if(usuariosEncontrados && usuariosEncontrados.length > 0){
+            const usuarioEncontrado = usuariosEncontrados[0]
+            return res.status(200).json({msg : `Usuário ${usuarioEncontrado.nome} autenticado`})
         }
         return res.status(400).json({erro : 'Usuário ou senha não encontrado'})
     }
