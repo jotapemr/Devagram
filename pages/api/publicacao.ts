97f018ef106480776ebe03cbf3ebeab6c0b904da
jotapemr <101481431+jotapemr@users.noreply.github.com>
2022-04-12 19:43:03 -0300

    atualização feed

--- pages/api/publicacao.ts
+++ pages/api/publicacao.ts
@@ -1,36 +1,36 @@
-import type {NextApiResponse} from 'next';
-import type { RespostaPadraoMsg } from '../../type/RespostaPadraoMsg';
+import type {NextApiResponse} from 'next'
+import type { RespostaPadraoMsg } from '../../type/RespostaPadraoMsg'
 import nc from 'next-connect';
-import {upload, uploadImagemCosmic} from '../../services/uploadImagemCosmic';
-import {conectarMongoDB} from '../../midllewares/conectarMongoDB';
-import {validarTokenJWT} from '../../midllewares/validarTokenJWT';
-import {PublicacaoModel} from '../../models/PublicacaoModel';
-import {UsuarioModel} from '../../models/UsuarioModels';
+import {upload, uploadImagemCosmic} from '../../services/uploadImagemCosmic'
+import {conectarMongoDB} from '../../midllewares/conectarMongoDB'
+import {validarTokenJWT} from '../../midllewares/validarTokenJWT'
+import {PublicacaoModel} from '../../models/PublicacaoModel'
+import {UsuarioModel} from '../../models/UsuarioModels'
 
 const handler = nc()
     .use(upload.single('file'))
     .post (async (req : any, res : NextApiResponse<RespostaPadraoMsg> | any) => {
         try{
             const {userId} = req.query;
-            const usuario = await UsuarioModel.findById(userId);
+            const usuario = await UsuarioModel.findById(userId)
             if(!usuario){
-                return res.status(400).json({erro : 'Usuário não encontrado'});
+                return res.status(400).json({erro : 'Usuário não encontrado'})
             }
 
             if(!req || !req.body){
-                return res.status(400).json({erro : 'Parâmetros de entrada não informados'});
+                return res.status(400).json({erro : 'Parâmetros de entrada não informados'})
             }
             const {descricao} = req?.body;
 
             if(!descricao || descricao.length < 2){
-                return res.status(400).json({erro : 'Descrição não e válida'});
+                return res.status(400).json({erro : 'Descrição não e válida'})
             }
     
             if(!req.file || !req.file.originalname){
-                return res.status(400).json({erro : 'Imagem é obrigatória'});
+                return res.status(400).json({erro : 'Imagem é obrigatória'})
             }
 
-            const image = await uploadImagemCosmic(req);
+            const image = await uploadImagemCosmic(req)
             const publicacao = {
                 idUsuario : usuario._id,
                 descricao,
@@ -39,13 +39,13 @@ const handler = nc()
             }
 
             usuario.publicacoes++;
-            await UsuarioModel.findByIdAndUpdate({_id : usuario._id}, usuario);
+            await UsuarioModel.findByIdAndUpdate({_id : usuario._id}, usuario)
 
-            await PublicacaoModel.create(publicacao);
-            return res.status(200).json({msg : 'Publicação criada com sucesso'});
+            await PublicacaoModel.create(publicacao)
+            return res.status(200).json({msg : 'Publicação criada com sucesso'})
         }catch(e){
             console.log(e);
-            return res.status(400).json({erro : 'Erro ao cadastrar publicação'});
+            return res.status(400).json({erro : 'Erro ao cadastrar publicação'})
         }
 })
 
