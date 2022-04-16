a396794ef994dd3461ba8825d47fd374e4a2d92c
jotapemr <101481431+jotapemr@users.noreply.github.com>
2022-04-16 13:48:38 -0300

    att

--- midllewares/conectarMongoDB.ts
+++ midllewares/conectarMongoDB.ts
@@ -1,25 +1,23 @@
-import type {NextApiRequest, NextApiResponse, NextApiHandler} from 'next'
-import mongoose from 'mongoose'
-import type {RespostaPadraoMsg} from '../type/RespostaPadraoMsg'
+import type {NextApiRequest, NextApiResponse, NextApiHandler} from 'next';
+import mongoose from 'mongoose';
+import type {RespostaPadraoMsg} from '../type/RespostaPadraoMsg';
 
-export const conectarMongoDB = (handler: NextApiHandler) =>
-    async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) =>{
+export const conectarMongoDB = (handler : NextApiHandler) =>
+    async (req: NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any[]>) => {
 
-        //checa se o banco está conectado, se estiver seguir para o proximo endpoint ou midlleware
-        if(mongoose.connections[0].readyState){
-            return handler(req, res)
-        }
-        //obter a variavel de ambiente preenchida do ENV
-        const {DB_CONEXAO_STRING} = process.env
-
-        //se a env tiver vazia aborta o sistema e avisa o dev
-        if(!DB_CONEXAO_STRING){
-            return res.status(500).json({erro : 'ENV de configuração do banco, não informado'})
-        }
-
-        mongoose.connection.on('connected', () => console.log('Banco de dados conectado'))
-        mongoose.connection.on('error', error => console.log('Ocorreu erro ao conectar banco de dados'))
-        await mongoose.connect(DB_CONEXAO_STRING)
-
-        return handler(req, res)
+    if(mongoose.connections[0].readyState){
+        return handler(req, res);
     }
+
+    const {DB_CONEXAO_STRING} = process.env;
+
+    if(!DB_CONEXAO_STRING){
+        return res.status(500).json({ erro : 'ENV de configuracao do banco, nao informado'});
+    }
+
+    mongoose.connection.on('connected', () => console.log('Banco de dados conectado'));
+    mongoose.connection.on('error', error => console.log(`Ocorreu erro ao conectar no banco: ${error}`));
+    await mongoose.connect(DB_CONEXAO_STRING);
+    
+    return handler(req, res);
+}
