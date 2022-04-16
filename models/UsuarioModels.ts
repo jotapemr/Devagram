e8995f68bbdf875165551b3229830f300c56a486
jotapemr <101481431+jotapemr@users.noreply.github.com>
2022-04-16 13:40:27 -0300

    att

--- models/UsuarioModels.ts
+++ models/UsuarioModels.ts
@@ -1,13 +1,13 @@
-import mongoose, {Schema} from 'mongoose'
+import mongoose, {Schema} from 'mongoose';
 
-const usuarioSchema = new Schema({
-    nome : {type : String, required : true},
-    email : {type : String, required : true},
-    senha : {type : String, required : true},
+const UsuarioSchema = new Schema({
+    nome: {type : String, required : true},
+    email: {type : String, required : true},
+    senha: {type : String, required : true},
     avatar : {type : String, required : false},
     seguidores : {type : Number, default : 0},
     seguindo : {type : Number, default : 0},
-    pulblicacoes : {type : Number, default : 0}
-})
+    publicacoes : {type : Number, default : 0},
+});
 
-export const UsuarioModel = (mongoose.models.usuarios || mongoose.model('usuarios', usuarioSchema))
+export const UsuarioModel = (mongoose.models.usuarios || mongoose.model('usuarios', UsuarioSchema));
